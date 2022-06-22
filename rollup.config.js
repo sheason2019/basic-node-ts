51cf4d7944ecb3bf584eecf6cce1dae7db762047
sheason <811627877@qq.com>
2022-06-22 17:53:02 +0800

    git init

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const babel = require('rollup-plugin-babel');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const pkg = require('./package.json');
+
+const extensions = ['.js', '.ts'];
+
+const resolve = function (...args) {
+  return path.resolve(__dirname, ...args);
+};
+
+module.exports = {
+  input: resolve('./main.js'),
+  output: {
+    file: resolve('./dist/', pkg.main), // 为了项目的统一性，这里读取 package.json 中的配置项
+    format: 'commonjs',
+  },
+  external: ['sequelize'],
+  plugins: [
+    nodeResolve({
+      extensions,
+      modulesOnly: true,
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions,
+    }),
+  ],
+};
